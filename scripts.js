508569e5d17f6f2b316096a96acb7e48367a377d
Muhammad Nur Abdullah <roll6009@gmail.com>
2025-05-29 21:26:22 +0200

    added a task updater

--- scripts.js
+++ scripts.js
@@ -20,3 +20,42 @@ const StorageManager = {
     return tasks ? JSON.parse(tasks) : [];
   },
 };
+
+/**
+ * Task Management Module
+ * Handles all operations related to task management
+ */
+const TaskManager = {
+  /**
+   * Adds a new task to the task list
+   * @param {Object} task - The task object to add
+   */
+  addTask: (task) => {
+    userTasks.push(task);
+    StorageManager.saveTasks(userTasks);
+    refreshTaskDisplay();
+  },
+
+  /**
+   * Updates an existing task
+   * @param {number} taskId - The ID of the task to update
+   * @param {Object} newData - The new task data
+   */
+  updateTask: (taskId, newData) => {
+    const taskIndex = userTasks.findIndex((task) => task.id === taskId);
+    if (taskIndex !== -1) {
+      userTasks[taskIndex] = { ...userTasks[taskIndex], ...newData };
+      StorageManager.saveTasks(userTasks);
+      refreshTaskDisplay();
+    }
+  },
+
+  deleteTask: function (taskId) {
+    const taskIndex = userTasks.findIndex((task) => task.id === taskId);
+    if (taskIndex !== -1) {
+      userTasks.splice(taskIndex, 1);
+      StorageManager.saveTasks(userTasks);
+      refreshTaskDisplay();
+    }
+  },
+};
